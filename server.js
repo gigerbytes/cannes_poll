8e5e5b2627e3c541fed623ae3746b47ac6471dc4
Bryan Giger <bryan@gigerbyt.es>
2018-03-26 16:45:52 +0200

    submit form to server works

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
 const app = express()
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 // db connection, no frontend, only respons
@@ -12,7 +15,10 @@ app.get('/', (req, res) => res.send('Hello World!'))
 // 2 actions
 
 // Accept form (form with data posted to express app)
-
+app.post('/vote', function(req, res){
+    console.log(req.body)
+    // res.send(200)
+})
 // Send result to deploy client (view over here)
 app.get('/poll', (req, res) => res.render("poll"))
 
