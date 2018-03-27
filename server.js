6ea3214d28fa981169a9aa2c2ca44e7f7c5b1f76
Bryan Giger <bryan@gigerbyt.es>
2018-03-27 19:39:50 +0200

    socketio integration

--- server.js
+++ server.js
@@ -2,34 +2,34 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+
 const app = express()
-require('dotenv').load();
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 var r = require('rethinkdb')
 var rdb = require("./lib/rethink")
 
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'))
+app.use("/public", express.static(__dirname + "/public"));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use(cors({credentials: true, origin: true}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
-// console.log()
-
-var connection = null;
-// r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
-//     if (err) throw err;
-//     connection = conn;
-//     r.db('test').tableCreate('authors').run(connection, function(err, result) {
-//     if (err) throw err;
-//     console.log(JSON.stringify(result, null, 2));
-// })
-// })
-
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/views/index.html')
+})
 // db connection, no frontend, only respons
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+    
 
 // 2 actions
 
@@ -51,4 +51,4 @@ app.get('/poll-update', function(req, res){
 
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+http.listen(3000, () => console.log('Example app listening on port 3000!'))
