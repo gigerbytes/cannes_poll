d6968feeb6dc4c31df50180a3a0e4e663376167f
Bryan Giger <bryan@gigerbyt.es>
2018-03-27 14:37:44 +0200

    Rethinkdb setup

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
-var cors = require('cors')
+const cors = require('cors')
 const app = express()
+require('dotenv').load();
+var r = require('rethinkdb')
+var rdb = require("./lib/rethink")
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
@@ -10,6 +15,18 @@ app.set('view engine', 'html');
 app.use(cors({credentials: true, origin: true}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
+
+// console.log()
+
+var connection = null;
+// r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
+//     if (err) throw err;
+//     connection = conn;
+//     r.db('test').tableCreate('authors').run(connection, function(err, result) {
+//     if (err) throw err;
+//     console.log(JSON.stringify(result, null, 2));
+// })
+// })
 
 app.get('/', (req, res) => res.send('Hello World!'))
 // db connection, no frontend, only respons
@@ -19,6 +36,7 @@ app.get('/', (req, res) => res.send('Hello World!'))
 // Accept form (form with data posted to express app)
 app.post('/vote', function(req, res){
     console.log(req.body)
+    rdb.save('vote',req.body)
     res.send("voted")
 })
 // Send result to deploy client (view over here)
