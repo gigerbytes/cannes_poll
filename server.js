b4534a249ec74dd9bd5a44b89078cf21ecc7ce3e
Bryan Giger <bryan@gigerbyt.es>
2018-03-27 10:33:59 +0200

    flash message, reset button

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => res.send('Hello World!'))
 // Accept form (form with data posted to express app)
 app.post('/vote', function(req, res){
     console.log(req.body)
-    res.send("hello")
+    res.send("success")
 })
 // Send result to deploy client (view over here)
 app.get('/poll', (req, res) => res.render("poll"))
